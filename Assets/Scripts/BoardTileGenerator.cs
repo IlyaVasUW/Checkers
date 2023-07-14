2598b4d8793d8441e3bbd10ca5eb42f333df9a28
Rusheel Dasari <rusheel@gmail.com>
2023-07-13 18:12:28 -0700

    Started code for board to generate tiles

--- Assets/Scripts/BoardTileGenerator.cs
+++ Assets/Scripts/BoardTileGenerator.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script1 : MonoBehaviour
+public class BoardTileGenerator : MonoBehaviour
 {
+    [SerializeField] public GameObject Tile;
     // Start is called before the first frame update
     void Start()
     {
